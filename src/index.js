9b7e25146ffb8a1ced1b13b7c751329506222471
hoainghia04 <112049040+hoainghia04@users.noreply.github.com>
2023-12-20 21:36:29 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,146 +1,134 @@
+import { config } from "dotenv";
 import { request } from "./request.mjs";
 import cheerio from "cheerio";
 import wait from "./wait.mjs";
 import {
-  REST,
-  Routes,
-  ApplicationCommandOptionType,
-  Client,
-  EmbedBuilder,
-  GatewayIntentBits,
+	REST,
+	Routes,
+	ApplicationCommandOptionType,
+	Client,
+	EmbedBuilder,
+	GatewayIntentBits,
 } from "discord.js";
 
-import config from './config.json' assert { type: "json" };
-
+config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const TOKEN = config.BOT_TOKEN;
-const CLIENT_ID = config.CLIENT_ID;
-const roleid = ["1129782219750256680", "1110776745092718655"];
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 client.on("ready", () => {
-  console.log('Bot Online!');
+	console.log('Bot Online!');
 });
 
 async function bypass(userhwid) {
-  const start_url =
-    "https://fluxteam.net/android/checkpoint/start.php?HWID=" + userhwid;
-  const commonheader = {
-    Referer: "https://linkvertise.com/",
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
-  };
-  await request(start_url, {
-    Referer: "https://fluxteam.net/",
-  });
-  await request(
-    "https://fluxteam.net/android/checkpoint/check1.php",
-    commonheader
-  );
-  await request(
-    "https://fluxteam.net/android/checkpoint/check2.php",
-    commonheader
-  );
-  await request(
-    "https://fluxteam.net/android/checkpoint/check3.php",
-    commonheader
-  );
-  const response = await request(
-    "https://fluxteam.net/android/checkpoint/main.php",
-    commonheader
-  );
-  const parsed = cheerio.load(response["data"]);
-  const key = parsed("body > main > code").text();
+	const start_url =
+		"https://fluxteam.net/android/checkpoint/start.php?HWID=" + userhwid;
+	const commonheader = {
+		Referer: "https://linkvertise.com/",
+		"User-Agent":
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
+	};
+	await request(start_url, {
+		Referer: "https://fluxteam.net/",
+	});
+	await request(
+		"https://fluxteam.net/android/checkpoint/check1.php",
+		commonheader
+	);
+	const response = await request(
+		"https://fluxteam.net/android/checkpoint/main.php",
+		commonheader
+	);
+	const parsed = cheerio.load(response["data"]);
+	const key = parsed("body > main > code").text();
 
-  return key;
+	return key;
 }
-const commandCooldowns = new Map();
+
 function extractHWIDFromURL(url) {
-  const regex = /HWID=([\w\d]+)/;
-  const match = url.match(regex);
-  return match ? match[1] : null;
+	const regex = /HWID=([\w\d]+)/;
+	const match = url.match(regex);
+	return match ? match[1] : null;
 }
 
 client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+// 	const allowedChannelId = '1174298298778132490';
+// 		if (!interaction.guild) {
+// 			await interaction.reply("Chỉ được sử dụng lệnh trong server. [Click vào đây để được vào server!](https://discord.gg/hngaming)");
+// 			return;
+// 			}
+//         if (interaction.channelId !== allowedChannelId) {
+//     const allowedChannel = interaction.guild.channels.cache.get(allowedChannelId);
+//     const channelLink = allowedChannel ? `<#${allowedChannel.id}>` : `channel ${allowedChannelId}`;
+//     const replyMessage = await interaction.reply(`Chỉ được dùng lệnh trong ${channelLink}.`);
 
-  if (interaction.commandName === "key") {
-    const link = interaction.options.get("link").value;
+//     setTimeout(async () => {
+//       await replyMessage.delete();
+//     }, 3000); 
+//     return;
+//   }
 
-    await interaction.deferReply();
+	if (!interaction.isChatInputCommand()) return;
 
-    try {
-      const userhwid = extractHWIDFromURL(link);
-      const isAdmin =
-      roleid.some((roleId) =>
-        interaction.member.roles.cache.has(roleId)
-      ) ||
-      interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR);
+	if (interaction.commandName === "ikey") {
+		const link = interaction.options.get("link").value;
 
-    if (!isAdmin && commandCooldowns.has(interaction.user.id)) {
-      const lastuse = commandCooldowns.get(interaction.user.id);
-      const currenttime = Date.now();
-      const cooldown = 60 * 60 * 1000;
+		await interaction.deferReply();
 
-      if (currenttime - lastuse < cooldown) {
-        const remainingTime = cooldown - (currenttime - lastuse);
-        const remainingMinutes = Math.ceil(remainingTime / (60 * 1000));
-        await interaction.editReply(
-          `Hãy đợi ${remainingMinutes} phút sau để thử lại.`
-        );
-        return;
-      }
-    }
-      const key = await bypass(userhwid);
-      const keyWithoutSpaces = key.replace(/\s+/g, "");
-      const embed = new EmbedBuilder()
-        .setColor('#ffffff')
-        .setTitle("*Copy Key Fluxus*")
-        .setDescription("```" + keyWithoutSpaces + "```")
-        .setThumbnail('https://cdn.discordapp.com/attachments/1174616840614457394/1185919580695171182/moon-red.gif?ex=65915cad&is=657ee7ad&hm=5fd86ad89f3b0c2e6ae8b0b286e76ff5370c0d4e0ec8bb3ea306c93f3b7c231d&')
-        .addFields(
-          {
-            name: "**Create By Inaki ❤**",
-            value: "**Subscribe My Channel.**\n [HN Gaming](https://www.youtube.com/channel/UC4FEU-bdsIZAJI7xOpUIxbw)",
-          },
-        );
+		try {
+			const userhwid = extractHWIDFromURL(link);
+			const key = await bypass(userhwid);
+			const keyWithoutSpaces = key.replace(/\s+/g, "");
+			const embed = new EmbedBuilder()
+				.setColor('#ffffff')
+				.setTitle("*Copy Key Fluxus*")
+				.setDescription("```" + keyWithoutSpaces + "```")
+				.setThumbnail('https://cdn.discordapp.com/attachments/1174616840614457394/1185919580695171182/moon-red.gif?ex=65915cad&is=657ee7ad&hm=5fd86ad89f3b0c2e6ae8b0b286e76ff5370c0d4e0ec8bb3ea306c93f3b7c231d&')
+				.addFields(
+				  {
+					name: "**Create By Inaki ❤**",
+					value: "**Subscribe My Channel.**\n [Click this is to go to youtube iNarki](https://www.youtube.com/channel/UC4FEU-bdsIZAJI7xOpUIxbw)",
+				  },
+				);
 
-      await interaction.editReply({ embeds: [embed] });
-    } catch (error) {
-      const errorMessage = await interaction.editReply(
-        "An error occurred while generating your key."
-      );
-      setTimeout(async () => {
-        await errorMessage.delete();
-      }, 3000);
-    }
-  }
+		await interaction.editReply({ embeds: [embed] });
+		} catch (error) {
+			const errorMessage = await interaction.editReply(
+				"Vui lòng thử lại."
+			);
+			setTimeout(async () => {
+				await errorMessage.delete();
+			}, 3000);
+		}
+	}
 });
 
 async function main() {
-  const commands = [
-    {
-      name: "key",
-      description: "Enter your link",
-      options: [
-        {
-          name: "link",
-          description: "enter your link",
-          type: ApplicationCommandOptionType.String,
-          required: true,
-        },
-      ],
-    }
-  ];
+	const commands = [
+		{
+			name: "ikey",
+			description: "Enter your link",
+			options: [
+				{
+					name: "link",
+					description: "enter your link",
+					type: ApplicationCommandOptionType.String,
+					required: true,
+				},
+			],
+		}
+	];
 
-  try {
-    console.log("Successfully added application (/) commands.");
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-    client.login(TOKEN);
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		console.log("Successfully added application (/) commands.");
+		await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+		client.login(TOKEN);
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 main();
